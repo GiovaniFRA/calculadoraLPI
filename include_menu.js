c06b0cacb17931c0505792cca94176407d93dd06
gzbrdr <gabrielzbardini@gmail.com>
2022-10-05 22:57:39 -0300

    vou me matar

--- include_menu.js
+++ include_menu.js
@@ -1,20 +1,16 @@
 $(document).ready(
-    function (){
-            
-        let menu="menu_ajax.html";//passando menu
+    function (){        
+        const menu="menu_ajax.html";//passando menu
         $.get(menu,
             function (html) {
                 $('.menu').html(html)
-            })
-        
-        let footer="footer_ajax.html"
+            }
+        )
+        const footer="footer_ajax.html"
         $.get(footer,
             function (html) {
                 $('footer').html(html)
-            })
-        
-        
-
+            }
+        )
     }
 );
-
