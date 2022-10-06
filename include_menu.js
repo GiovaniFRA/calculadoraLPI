2c0a02bb9ab131d35ff8e7e4ab695fa0893a3a2e
gzbrdr <gabrielzbardini@gmail.com>
2022-10-06 01:06:30 -0300

    asdsadasd

--- include_menu.js
+++ include_menu.js
@@ -1,16 +1,31 @@
 $(document).ready(
-    function (){        
-        const menu="menu_ajax.html";//passando menu
+    function (){
+            
+        let menu="menu_ajax.html";//passando menu
         $.get(menu,
             function (html) {
+                let i=0
                 $('.menu').html(html)
-            }
-        )
-        const footer="footer_ajax.html"
+                $('#sublist').click(function(){
+                    
+                    if(i%2==0){
+                        $("#submenu").css({"display":"flex","background-color":"#4d68e4"})
+                        $("#sublist").css({"background-color":"#4d68e4"})
+
+                    }else{
+                        $("#sublist").css({"background-color":"#7c90e7"})
+                        $("#submenu").css("display","none")
+                    }
+
+                    i++;
+                })
+            })
+        
+        let footer="footer_ajax.html"
         $.get(footer,
             function (html) {
                 $('footer').html(html)
-            }
-        )
+            })
+        
     }
 );
